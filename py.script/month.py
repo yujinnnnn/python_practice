affc787c2877a2abaaa8b688cbdf1a684ecb46aa
yujinnnnn <67722139+yujinnnnn@users.noreply.github.com>
2020-07-28 18:09:29 +0900

    Delete month.py

--- py.script/month.py
+++
@@ -1,9 +0,0 @@
-month = int(input("Enter month >> "))
-if month ==2:
-    print(month, "월 :: 28일")
-elif month in [ 1,3,5,7,8,10,12]:
-        print(month, "월 :: 31일")
-elif month in [4,6,9,11]:
-        print(month, "월 :: 30일")
-else:
-    print("잘못입력")
